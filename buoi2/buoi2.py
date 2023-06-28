45686973ebb349c3501ece3b5eb5bfeeee778581
HaTai2901 <Hatantai19@gmail.com>
2023-06-29 00:13:09 +0700

    baitap1.py - update

--- buoi2/buoi2.py
+++ buoi2/buoi2.py
@@ -1,2 +1,21 @@
-print("hello")
-s = 1
+'''
+    ôn tập
+    nhập vào tên, tuổi của 1 sinh viên:
+    - in ra màn hình tên và tuổi của sinh viên đó
+    - kiểm tra kiểu dữ liệu của 2 biến tên tuổi và in ra trên 1 dòng cách nhau bởi dấu ,
+    - định nghĩa cho 2 biến num1 num2 có cùng giá trị bất kì, kiểm tra 2 biến đó thuộc kiểu dữ liệu gì
+'''
+# # cau1
+# x = input('nhap ten sinh vien: ')
+# a = input(int('nhap tuoi: '))
+# print(f"ten sinh vien: {x}, {a} tuoi")
+
+# # cau2
+# print(type(x), type(a))
+
+# cau3
+num1 = 19
+num2 = 19
+
+print(type(num1))
+print(type(num2))
