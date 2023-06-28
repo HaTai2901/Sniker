9a2969daf907774a5e6d5790ccf80ac546b437a8
HaTai2901 <Hatantai19@gmail.com>
2023-06-28 21:19:29 +0700

    first commit

---
+++ buoi1.py
@@ -0,0 +1,38 @@
+"""
+    bài tập:
+    - cho biết kiểu của các giá trị sau: -50, 4.98908, .5, 4+5j
+    - định nghĩa 2 biến num1, num2 có cùng giá trị 1
+    - nhập vào tên và tuổi và hiển thị ra màn hình, cách nhau bởi dấu phẩy
+    - nhập vào 1 số nguyên, số thực, chuỗi và in các giá trị của chúng trên các dòng riêng biệt
+"""
+
+
+# x = -50
+# print(type(x))
+# x = 4.98908 
+# print(type(x))
+# x = .5
+# print(type(x))
+# x = 4+5j
+# print(type(x))
+
+# print("-"*30)
+
+# num1 = 1
+# num2 = 2
+# print({} == {})
+
+# print("-"*30)
+
+# x = input("họ và tên: ")
+# y = input("may tuổi : ")
+# print(x, y)
+
+print("-"*30)
+
+x = input("nhap mot so nguyen: ")
+y = input("nhap mot so thuc: ")
+s = input("nhap mot so chuoi: ")
+print(type(x))
+
+
